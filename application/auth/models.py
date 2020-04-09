48c7aaddbfeea65e4540281d83594fe9b61309ce
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2020-04-09 12:04:32 +0300

    bcrypt otettu käyttöön

--- application/auth/models.py
+++ application/auth/models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "account"
   
     name = db.Column(db.String(144), nullable=False)
-    username = db.Column(db.String(144), nullable=False)
+    username = db.Column(db.String(144), nullable=False, unique=True)
     password = db.Column(db.String(144), nullable=False)
 
     events = db.relationship("Event", backref='account', lazy=True)
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app, db
+from application import app, db, bcrypt
 from application.auth.models import User
 from application.auth.forms import LoginForm, RegistrationForm
 
@@ -12,15 +12,15 @@ def auth_login():
 
     form = LoginForm(request.form)
 
-    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    user = User.query.filter_by(username=form.username.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
-                               error = "Käyttäjää ei löydy tai salasana on väärin")
-
-
-    print("Tervetuloa, " + user.name + "!")
-    login_user(user)
-    return redirect(url_for("index"))  
+                               error = "Käyttäjää ei löydy")
+    else:
+        if bcrypt.check_password_hash(user.password, form.password.data):
+            print("Tervetuloa, " + user.name + "!")
+            login_user(user)
+            return redirect(url_for("index")) 
 
 @app.route("/auth/logout")
 def auth_logout():
@@ -36,8 +36,13 @@ def auth_registration():
 
     if not form.validate():
         return render_template("auth/registrationform.html", form = form)
+    else:
+        user = User.query.filter_by(username=form.username.data).first()
 
-    user = User(form.name.data, form.username.data, form.password.data)
+        if user:
+            return render_template("auth/registrationform.html", form = form, error ="Haluamasi tunnus on jo varattu.")
+
+    user = User(form.name.data, form.username.data, bcrypt.generate_password_hash(form.password.data).decode('utf-8'))
 
     db.session().add(user)
     db.session().commit()
